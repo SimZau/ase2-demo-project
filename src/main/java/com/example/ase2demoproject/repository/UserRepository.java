d8c01cfd674bc8dd143481b607cffc9221c0f466
Simon Zaugg <zaugsi1@students.zhaw.ch>
2022-03-09 09:20:51 +0100

    Fix MVC

--- src/main/java/com/example/ase2demoproject/repository/UserRepository.java
+++ src/main/java/com/example/ase2demoproject/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.ase2demoproject.repository;
 
-import java.util.List;
 import com.example.ase2demoproject.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findByName(String name);
